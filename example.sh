79cf4546c2d37bd142825594f232fe16c955147d
ceteri <ceteri@gmail.com>
2012-05-26 17:20:39 -0700

    CC licenses

--- example.sh
+++ example.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# by Liber 118
+# http://liber118.com/
+# licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License
+# http://creativecommons.org/licenses/by-sa/3.0/
+
+
 export LC_ALL='C'
 
 
